ffcb86bd62df32c0be845d68ed06dc1edfe17063
gabe <ellergabriel13@gmail.com>
2024-09-04 15:50:17 -0700

    basic django API setup

---
+++ backend/guardian_website/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Guardian
+
+class GuardianSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guardian
+        fields = ('fName', 'lName')
--- backend/guardian_website/views.py
+++ backend/guardian_website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import GuardianSerializer
+from .models import Guardian
 
 # Create your views here.
+class GuardianView(viewsets.ModelViewSet):
+    serializer_class = GuardianSerializer
+    queryset = Guardian.objects.all()
